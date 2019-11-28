f7cf0f84cfff8316c08ec0ac267cf1af0d20605e
dung <trungdung1311@gmail.com>
2019-11-28 16:05:02 +0700

    update

--- source/screens/ADM002.tsx
+++ source/screens/ADM002.tsx
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import { NavigationScreenProp, NavigationState } from "react-navigation";
 import { Text, View, TextInput, Button, Alert, TouchableOpacity, SafeAreaView } from 'react-native';
 import { BaseButton } from 'react-native-gesture-handler';
+import { SearchBar } from 'react-native-elements';
+import { Icon } from 'react-native-elements'
 
-export class ADM002 extends Component<{ navigation: NavigationScreenProp<NavigationState> }> {
+export class ADM002 extends Component<{ navigation: NavigationScreenProp<NavigationState> }, { searchKey: string }> {
     constructor(props: { navigation: NavigationScreenProp<NavigationState> }) {
         super(props);
+        this.state = {
+            searchKey: ''
+        };
     }
     render() {
+
         return (
             <SafeAreaView style={{ flex: 1 }}>
-               
+                <SearchBar
+                    placeholder="Type Here..."
+                    
+                    onChangeText={(text) => this.setState({ searchKey: text })}
+                    value={this.state.searchKey}
+                />
             </SafeAreaView>
         );
     }
